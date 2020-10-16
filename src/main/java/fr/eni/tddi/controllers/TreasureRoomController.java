13d2c63cb6c646340c142b9feaaf620c41873c2e
stephane <sgobin@campus-eni.fr>
2020-10-16 15:36:17 +0200

    enonce-v5

--- src/main/java/fr/eni/tddi/controllers/TreasureRoomController.java
+++ src/main/java/fr/eni/tddi/controllers/TreasureRoomController.java
@@ -1,10 +1,9 @@
 package fr.eni.tddi.controllers;
 
-import org.springframework.stereotype.Controller;
 
 import fr.eni.tddi.services.TreasureService;
 
-@Controller("salleTresor")
+
 public class TreasureRoomController {
 	
 	private TreasureService tresorService;
